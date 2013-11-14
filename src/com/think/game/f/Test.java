69fd35d5fe5d9ff9fee6cf1cef8c9d65ca12fdfa
Yicha_chengzhenhua <tianzhihen5236@gmail.com>
2013-11-14 13:10:19 +0800

    判断有没有可消除的方格

--- src/com/think/game/f/Test.java
+++ src/com/think/game/f/Test.java
@@ -1,5 +1,6 @@
 package com.think.game.f;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -33,5 +34,75 @@ public class Test {
 			game.printLayout();
 		} while (!c.equals("exit"));
 		scan.close();
+	}
+	/**
+	 * 判断是否有可以消除的小方块,true没有,false有
+	 * @param rcs
+	 * @return 
+	 */
+	public static boolean checkGameOver(int[][] rcs) {
+		int rows = rcs.length;
+		int cols = rcs[0].length;
+		HashMap<Integer, Integer> exitHash = new HashMap<Integer, Integer>();
+		boolean flag = true;
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < cols; j++) {
+				if(rcs[i][j] == 0) {
+					int tx = j;
+					int ty = i;
+					// 左边的节点
+					do {
+						--tx;
+					} while (tx > 0 && rcs[tx][ty] == 0);
+					if (tx >= 0) {
+						exitHash.put(rcs[tx][ty], 0);
+					}
+					// 右边的节点
+					tx = j;
+					ty = i;
+					do {
+						++tx;
+					} while (tx < cols - 1 && rcs[tx][ty] == 0);
+					if (tx < cols) {
+						if(exitHash.get(rcs[tx][ty]) == null) {
+							exitHash.put(rcs[tx][ty], 0);
+						} else {
+							flag = false;
+							break;
+						}
+					}
+					// 上边的节点
+					tx = j;
+					ty = i;
+					do {
+						--ty;
+					} while (ty > 0 && rcs[tx][ty] == 0);
+					if (ty >= 0) {
+						if(exitHash.get(rcs[tx][ty]) == null) {
+							exitHash.put(rcs[tx][ty], 0);
+						} else {
+							flag = false;
+							break;
+						}
+					}
+					// 下边的节点
+					tx = j;
+					ty = i;
+					do {
+						++ty;
+					} while (ty < rows - 1 && rcs[tx][ty] == 0);
+					if (ty < rows) {
+						if(exitHash.get(rcs[tx][ty]) == null) {
+							exitHash.put(rcs[tx][ty], 0);
+						} else {
+							flag = false;
+							break;
+						}
+					}
+					exitHash.clear();
+				}
+			}
+		}
+		return flag;
 	}
 }
